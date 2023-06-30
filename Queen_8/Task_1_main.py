7fc1708409161b316d789a1974af6fcc29232159
Vadim <fishermanvadik@gmail.com>
2023-06-30 17:46:43 +0300

    Time

---
+++ Queen_8/Task_1_main.py
@@ -0,0 +1,9 @@
+import Task_1_func
+
+
+def t1_start():
+    print(Task_1_func.date_check(input('Введите существующую дату в правильном формате (dd.mm.YYYY) - ')))
+
+
+if __name__ == '__main__':
+    t1_start()
